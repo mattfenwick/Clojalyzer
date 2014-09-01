1b8bb4a797c8f0930dc7380c3a8b3f26319e3c9b
Matt Fenwick <mfenwick100@gmail.com>
2014-09-01 15:58:55 -0400

    - instructions  - styles  - formatting

--- build/bundle.js
+++ build/bundle.js
@@ -20,8 +20,8 @@ function formatRow(rowData) {
     var pos = rowData.position;
     return H.tr({},
                 H.td({}, rowData.name),
-                H.td({}, pos[0] + ', ' + pos[1]),
-                H.td({}, rowData.doc));
+                H.td({}, pos[0] + ''),
+                H.td({}, H.pre({}, '\n' + rowData.doc)));
 }
 
 Analyzer.prototype.success = function(response) {
@@ -31,9 +31,9 @@ Analyzer.prototype.success = function(response) {
         return;
     }
     var header = H.tr({},
-                      H.td({}, 'function name'),
-                      H.td({}, 'line and column'),
-                      H.td({}, 'doc string')),
+                      H.th({}, 'Function name'),
+                      H.th({}, 'Line number'),
+                      H.th({}, 'Documentation string')),
         rows = [header].concat(c.value.map(formatRow)),
         table = elem('table', {}, rows);
     this.div.append(G.serialize.serialize(table));
@@ -43,29 +43,16 @@ Analyzer.prototype.displayFile = function(response, status) {
     console.log('got a response -- ' + status);
     this.div.empty();
     switch (status) {
-        case 'request error':
-        case 'api error':
-        case 'data error':
-            this.div.append(status);
-            break;
         case 'success':
             this.success(response);
             break;
+        case 'request error':
+        case 'api error':
+        case 'data error':
         default:
-            throw new Error('unexpected status: ' + status + ' ' + JSON.stringify(response));
+            this.div.append(status);
+            break;
     }
-};
-
-Analyzer.prototype.notify = function() {
-    // send event off to listeners
-    var args = Array.prototype.slice.call(arguments);
-    this.listeners.forEach(function(f) {
-        f.apply(null, args);
-    });
-};
-
-Analyzer.prototype.listen = function(l) {
-    this.listeners.push(l);
 };
 
 
--- js/analyzer.js
+++ js/analyzer.js
@@ -19,8 +19,8 @@ function formatRow(rowData) {
     var pos = rowData.position;
     return H.tr({},
                 H.td({}, rowData.name),
-                H.td({}, pos[0] + ', ' + pos[1]),
-                H.td({}, rowData.doc));
+                H.td({}, pos[0] + ''),
+                H.td({}, H.pre({}, '\n' + rowData.doc)));
 }
 
 Analyzer.prototype.success = function(response) {
@@ -30,9 +30,9 @@ Analyzer.prototype.success = function(response) {
         return;
     }
     var header = H.tr({},
-                      H.td({}, 'function name'),
-                      H.td({}, 'line and column'),
-                      H.td({}, 'doc string')),
+                      H.th({}, 'Function name'),
+                      H.th({}, 'Line number'),
+                      H.th({}, 'Documentation string')),
         rows = [header].concat(c.value.map(formatRow)),
         table = elem('table', {}, rows);
     this.div.append(G.serialize.serialize(table));
@@ -42,29 +42,16 @@ Analyzer.prototype.displayFile = function(response, status) {
     console.log('got a response -- ' + status);
     this.div.empty();
     switch (status) {
-        case 'request error':
-        case 'api error':
-        case 'data error':
-            this.div.append(status);
-            break;
         case 'success':
             this.success(response);
             break;
+        case 'request error':
+        case 'api error':
+        case 'data error':
         default:
-            throw new Error('unexpected status: ' + status + ' ' + JSON.stringify(response));
+            this.div.append(status);
+            break;
     }
-};
-
-Analyzer.prototype.notify = function() {
-    // send event off to listeners
-    var args = Array.prototype.slice.call(arguments);
-    this.listeners.forEach(function(f) {
-        f.apply(null, args);
-    });
-};
-
-Analyzer.prototype.listen = function(l) {
-    this.listeners.push(l);
 };
 
 
